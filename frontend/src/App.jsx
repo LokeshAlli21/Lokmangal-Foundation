75b9763df9b451cb192b0104c160040c486a52f6
Lokesh <lokeshalli1807@gmail.com>
2025-04-06 19:03:18 +0530

    test to deploy 8

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,9 @@ import moment from 'moment';
 window.moment = moment;
 
 import './css/style.css';
-// import './js/jquery-ui';
-import './js/slick';
-import './js/select-opt';
+import './js/jquery-ui';
+// import './js/slick';
+// import './js/select-opt';
 import './js/Chart';
 import './js/mail';
 import './js/custom';
