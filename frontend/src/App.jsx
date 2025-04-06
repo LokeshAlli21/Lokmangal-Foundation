e36373563d9bf3b40e28821626a7707e81738292
Lokesh <lokeshalli1807@gmail.com>
2025-04-06 18:56:00 +0530

    test to deploy 4

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,15 @@
 import moment from 'moment';
 window.moment = moment;
 
-import './css/style.css';
-import './js/jquery-ui';
-import './js/slick';
-import './js/select-opt';
-import './js/Chart';
-import './js/mail';
-import './js/custom';
-import 'animate.css';
-import './css/animate.min.css';
+// import './css/style.css';
+// import './js/jquery-ui';
+// import './js/slick';
+// import './js/select-opt';
+// import './js/Chart';
+// import './js/mail';
+// import './js/custom';
+// import 'animate.css';
+// import './css/animate.min.css';
 
 import { Outlet } from "react-router-dom";
 import Header from './components/Header';
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import commonjs from '@rollup/plugin-commonjs';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), commonjs()],
-  build: {
-    commonjsOptions: {
-      transformMixedEsModules: true,
-    },
-  },
-});
+  plugins: [react()],
+})
