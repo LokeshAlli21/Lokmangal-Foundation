b92288eab24f8ab50de8cad6eac2bc30f623352b
Lokesh <lokeshalli1807@gmail.com>
2025-04-16 18:08:07 +0530

    update UI 4

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -484,7 +484,7 @@ function Home() {
   <section>
     <div className="str ">
     
-      <div className="hom-head " style={homHeadStyle}>
+      <div className="hom-head " style={{marginTop: 0}}>
         <div className="container">
           <div className="row">
             <div className="hom-ban">
